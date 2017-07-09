ab54cede032b7ec073f6f76aef8b0372bafd33d1
Karma Maker <karmamaker.dev@gmail.com>
2017-07-10 01:53:40 +0300

    More pleasant colour scheme + error image

--- app/src/main/java/space/serenity/scrollme/binders/ImageBinder.kt
+++ app/src/main/java/space/serenity/scrollme/binders/ImageBinder.kt
@@ -1,11 +1,11 @@
 package space.serenity.scrollme.binders
 
-import android.graphics.Color
 import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_image.view.*
 import space.serenity.scrollme.R
 import space.serenity.scrollme.model.FlickrPhoto
+
 
 /**
  * Created by karmamaker on 31/05/2017.
@@ -17,7 +17,8 @@ class ImageBinder(parent: ViewGroup) : Binder(R.layout.item_image, parent) {
 
         Picasso.with(itemView.context)
                 .load("http://farm${data.farm}.static.flickr.com/${data.server}/${data.id}_${data.secret}.jpg")
-                .error(R.drawable.abc_btn_colored_material)
+                .error(R.drawable.no_internet)
+                .placeholder(R.color.colorAccent)
                 .resize(240, 240)
                 .centerCrop()
                 .into(itemView.img)
